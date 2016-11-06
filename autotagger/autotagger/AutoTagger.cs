64c15fda9082da12ac5ccfb9884c6dae45367abe
Pavels Nikolajevs <paavels@gmail.com>
2016-11-06 22:40:45 +0200

    .

---
+++ autotagger/autotagger/AutoTagger.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace autotagger
+{
+    public class AutoTagger
+    {
+        private TagManager tagManager;
+
+        public AutoTagger()
+        {
+            tagManager = new TagManager();
+            Console.WriteLine("Tags read");
+        }
+
+        private void PrintHelp()
+        {
+            Console.WriteLine("Usages:");
+            Console.WriteLine("autotagger parse [term]");
+            Console.WriteLine("autotagger tags - show existing tags");
+        }
+
+        private void Parse(string[] _args)
+        {
+            if (_args.Length == 0)
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine("autotagger parse [term]");
+                Console.WriteLine("Where term is you want to autotag");
+                return;
+            }
+
+            string term = string.Join(" ", _args.Select(_s => _s.ToString()));
+
+        }
+
+        private void ShowTags()
+        {
+            List<Tag> tags = tagManager.Get();
+
+            Console.WriteLine($"Found {tags.Count} tags");
+            foreach (Tag tag in tags)
+            {
+                Console.WriteLine(tag.Get());
+            }
+        }
+
+
+        public void Run(string _mode, string[] _args)
+        {
+            switch (_mode)
+            {
+                case "parse":
+                    Parse(_args);
+                    break;
+                case "tags":
+                    ShowTags();
+                    break;
+                default:
+                    PrintHelp();
+                    break;
+            }
+
+        }
+
+        ~AutoTagger()
+        {
+            Console.WriteLine("Saving tags");
+            tagManager.Save();
+
+            Console.WriteLine("Application exiting");
+        }
+    }
+}
--- autotagger/autotagger/Program.cs
+++ autotagger/autotagger/Program.cs
@@ -1,40 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace autotagger
 {
-    class Program
+    internal class Program
     {
-        static void printHelp()
+        private static void Main(string[] _args)
         {
-            Console.WriteLine("Usages:");
-            Console.WriteLine("autotagger explain [term] [meaning]");
-            Console.WriteLine("autotagger tags - show existing tags");
-            Console.WriteLine("autotagger explanations - show explanations");
-        }
-
-        static void explain()
-        {
-            
-        }
-
-        static void showTags()
-        {
-            TagManager tagManager = new TagManager();
-            List<Tag> tags = tagManager.Get();
-
-            Console.WriteLine(string.Format("Found {0} tags", tags.Count));
-            foreach (Tag tag in tags)
-            {
-                Console.WriteLine(tag.Get());
-            }
-        }
-
-        static void Main(string[] args)
-        {
-            if(args.Length == 0) printHelp();
-
-            showTags();
+            AutoTagger app = new AutoTagger();
+            //app.Run("parse", _args.Skip(1).ToArray());
+            app.Run("parse", new[] { "MMGF2 MacBook Air 13\" i5 DC1.6GHz/ 8GB/ 128GB flash/ Intel HD 6000/ INT" });
+            app.Run("parse", new[] { "MK482 Apple iMac 27\" Retina 5K QC i5 3.3GHz/ 8GB/ 2TB Fusion/ AMD Radeon R9 M395 2GB/ Int" });
+            app.Run("parse", new[] { "Mobilais telefons Coolpad MAX 5.5\" 64GB - Champagne" });
 
             Console.ReadKey();
         }
